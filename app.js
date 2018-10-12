e1d78c7f1cd4324144f80440e561ec0dffde05bd
poorani11 <poorani.mgr@gmail.com>
2018-10-13 04:07:17 +0530

    Added css for the tabs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // MODULE
-var itunesApp = angular.module('itunesApp', ['ngRoute', 'ngResource','ngMaterial','ngAnimate','ngAria']);
+var itunesApp = angular.module('itunesApp', ['ngRoute', 'ngResource', 'ngMaterial', 'ngAnimate', 'ngAria']);
 
 // ROUTES
 itunesApp.config(function ($routeProvider) {
@@ -40,6 +40,31 @@ itunesApp.service('artistService', ['$resource', '$q', function ($resource, $q) 
     self.getArtistData = function () {
         return self.data;
     }
+
+    self.getData = function (artistName) {
+
+        return $q(function (resolve, reject) {
+
+            var resObj = $resource("http://itunes.apple.com/search");
+            var resp = resObj.get({
+                term: artistName,
+                limit: 1
+            });
+            resp.$promise.then(function (data) {
+                console.log('Got data');
+                self.data = data;
+                resolve('success')
+            }, function (err) {
+                console.log(err);
+                console.log('error');
+                reject('error')
+            });
+        })
+
+    }
+    self.getTabData = function () {
+        return self.data;
+    }
 }]);
 
 //CONTROLLERS
@@ -53,9 +78,27 @@ itunesApp.controller('homeController', ['$scope', 'artistService', function ($sc
                 var artistData = artistService.getArtistData();
                 $scope.artistData = artistData.results;
                 $scope.gotData = true;
-            console.log($scope.artistData)
 
             })
     }
+
+    $scope.onTabSelected = function (tab) {
+        var artistName = tab.artistName.replace(/\s/g, '');
+        artistService.getData(artistName)
+            .then(function (result) {
+                var tabData = artistService.getTabData();
+                $scope.tabData = tabData.results[0];
+                $scope.tabData.description = strip($scope.tabData.description);
+                console.log($scope.tabData)
+
+            })
+    }
+
+    function strip(html) {
+        var tmp = document.implementation.createHTMLDocument("New").body;
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || "";
+    }
+
 
 }]);
